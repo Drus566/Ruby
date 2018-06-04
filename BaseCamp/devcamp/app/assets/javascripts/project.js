bcb70e3497e877e58549a3b3e4546a18252960c8
Drus566 <ahdpeu566@mail.ru>
2018-06-04 12:54:21 +0500

    Basecamp

---
+++ BaseCamp/devcamp/app/assets/javascripts/project.js
@@ -0,0 +1,35 @@
+console.log('ggwp');
+document.addEventListener("click", function(){
+    var openProjectButton = document.getElementById('new-project-button')
+    var projectPopover = document.getElementById('new-project-popover')
+
+    if (openProjectButton && projectPopover){
+        
+        openProjectButton.addEventListener('click', function(){
+            return projectPopover.classList.contains('is-hidden') ? projectPopover.classList.remove('is-hidden') : null
+        }, false)
+
+        var cancelProjectPopover = document.getElementById('cancel-project-popover')
+        cancelProjectPopover.addEventListener('click', function(){
+            return projectPopover.classList.add('is-hidden')
+        },false)
+    }
+})
+
+
+document.addEventListener("turbolinks:load", function(){
+    var openProjectButton = document.getElementById('new-project-button')
+    var projectPopover = document.getElementById('new-project-popover')
+
+    
+        
+        openProjectButton.addEventListener('click', function(){
+            return projectPopover.classList.contains('is-hidden') ? projectPopover.classList.remove('is-hidden') : null
+        }, false)
+
+        var cancelProjectPopover = document.getElementById('cancel-project-popover')
+        cancelProjectPopover.addEventListener('click', function(){
+            return projectPopover.classList.add('is-hidden')
+        },false)
+    
+})
