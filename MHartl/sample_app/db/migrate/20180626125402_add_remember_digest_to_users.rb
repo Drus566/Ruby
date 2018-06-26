9db032e80e1b3ae72f3de542eabdcff88b2acc4b
Drus566 <ahdpeu566@mail.ru>
2018-06-26 19:07:18 +0500

    Activating Account

--- MHartl/sample_app/db/migrate/20180626125402_add_remember_digest_to_users.rb
+++ MHartl/sample_app/db/migrate/20180626125402_add_remember_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddRememberDigestToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :admin, :boolean, default: false
+    add_column :users, :remember_digest, :string
   end
 end
