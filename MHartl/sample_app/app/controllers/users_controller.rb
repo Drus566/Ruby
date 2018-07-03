a88578843f640f7196592acbbee0fc3abd2107a1
Drus566 <ahdpeu566@mail.ru>
2018-07-03 21:52:20 +0500

    GGWP

--- MHartl/sample_app/app/controllers/users_controller.rb
+++ MHartl/sample_app/app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])  
     redirect_to root_url and return unless @user.activated
   end
   
@@ -46,22 +47,16 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted"
     redirect_to users_url
   end
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
   
-  # Confirms the correct user.
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+  
 
   private
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
